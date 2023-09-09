af238ccdbcacb0afed1d030c0085ffa5c52d2545
David Hadrianus <95863744+hadrianus-dev@users.noreply.github.com>
2023-09-09 18:12:04 +0100

    move production code their own file
    move production code their own file

--- src/presentation/routes/login-route.spec.js
+++ src/presentation/routes/login-route.spec.js
@@ -1,40 +1,5 @@
-class LoginRouter {
-  route (httpRequest) {
-    if (!httpRequest || !httpRequest.body) {
-      return HttpResponse.serverError()
-    } else {
-      const { email, password } = httpRequest
-      if (!email) {
-        return HttpResponse.badRequest('email')
-      }
-      if (!password) {
-        return HttpResponse.badRequest('password')
-      }
-    }
-  }
-}
-
-class HttpResponse {
-  static badRequest (paramName) {
-    return {
-      statusCode: 400,
-      body: new MissingParamError(paramName)
-    }
-  }
-
-  static serverError () {
-    return {
-      statusCode: 500
-    }
-  }
-}
-
-class MissingParamError extends Error {
-  constructor (paramName) {
-    super(`Missing param: ${paramName}`)
-    this.name = 'MissingParamError'
-  }
-}
+const LoginRouter = require('./LoginRouter')
+const MissingParamError = require('../Helpers/MissingParamError')
 
 describe('LoginRouteSpec', () => {
   test('Shoud return 400 if not email is provided', () => {
